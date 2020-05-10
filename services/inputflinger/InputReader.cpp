57d4a097aa3d1685fd62028b3e8a086c895b4d35
Mrinal Ghosh <mg712702@gmail.com>
2020-05-10 20:59:06 +0530

    Revert "Forward port 'Swap volume buttons' (2/3)"
    This reverts commit 967a7f7a165624e930eea10217754e3cdb7d54a1.
    
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- services/inputflinger/InputReader.cpp
+++ services/inputflinger/InputReader.cpp
@@ -124,9 +124,9 @@ static inline const char* toString(bool value) {
 }
 
 static int32_t rotateValueUsingRotationMap(int32_t value, int32_t orientation,
-        const int32_t map[][4], size_t mapSize, int32_t rotationMapOffset) {
+        const int32_t map[][4], size_t mapSize) {
     if (orientation != DISPLAY_ORIENTATION_0) {
-        for (size_t i = rotationMapOffset; i < mapSize; i++) {
+        for (size_t i = 0; i < mapSize; i++) {
             if (value == map[i][0]) {
                 return map[i][orientation];
             }
@@ -138,16 +138,6 @@ static int32_t rotateValueUsingRotationMap(int32_t value, int32_t orientation,
 static const int32_t keyCodeRotationMap[][4] = {
         // key codes enumerated counter-clockwise with the original (unrotated) key first
         // no rotation,        90 degree rotation,  180 degree rotation, 270 degree rotation
-
-        // volume keys - tablet
-        { AKEYCODE_VOLUME_UP,   AKEYCODE_VOLUME_UP,   AKEYCODE_VOLUME_DOWN, AKEYCODE_VOLUME_DOWN },
-        { AKEYCODE_VOLUME_DOWN, AKEYCODE_VOLUME_DOWN, AKEYCODE_VOLUME_UP,   AKEYCODE_VOLUME_UP },
-
-        // volume keys - phone or hybrid
-        { AKEYCODE_VOLUME_UP,   AKEYCODE_VOLUME_DOWN, AKEYCODE_VOLUME_DOWN, AKEYCODE_VOLUME_UP },
-        { AKEYCODE_VOLUME_DOWN, AKEYCODE_VOLUME_UP,   AKEYCODE_VOLUME_UP,   AKEYCODE_VOLUME_DOWN },
-
-        // dpad keys - common
         { AKEYCODE_DPAD_DOWN,   AKEYCODE_DPAD_RIGHT,  AKEYCODE_DPAD_UP,     AKEYCODE_DPAD_LEFT },
         { AKEYCODE_DPAD_RIGHT,  AKEYCODE_DPAD_UP,     AKEYCODE_DPAD_LEFT,   AKEYCODE_DPAD_DOWN },
         { AKEYCODE_DPAD_UP,     AKEYCODE_DPAD_LEFT,   AKEYCODE_DPAD_DOWN,   AKEYCODE_DPAD_RIGHT },
@@ -188,11 +178,11 @@ static int32_t stemKeyRotationMap[][2] = {
 static const size_t stemKeyRotationMapSize =
         sizeof(stemKeyRotationMap) / sizeof(stemKeyRotationMap[0]);
 
-static int32_t rotateKeyCode(int32_t keyCode, int32_t orientation, int32_t rotationMapOffset) {
+static int32_t rotateKeyCode(int32_t keyCode, int32_t orientation) {
     keyCode = rotateStemKey(keyCode, orientation,
             stemKeyRotationMap, stemKeyRotationMapSize);
     return rotateValueUsingRotationMap(keyCode, orientation,
-            keyCodeRotationMap, keyCodeRotationMapSize, rotationMapOffset);
+            keyCodeRotationMap, keyCodeRotationMapSize);
 }
 
 static void rotateDelta(int32_t orientation, float* deltaX, float* deltaY) {
@@ -2252,13 +2242,6 @@ void KeyboardInputMapper::configure(nsecs_t when,
             mViewport = config->getDisplayViewportByType(ViewportType::VIEWPORT_INTERNAL);
         }
     }
-
-    if (!changes || (changes & InputReaderConfiguration::CHANGE_VOLUME_KEYS_ROTATION)) {
-        // mode 0 (disabled) ~ offset 4
-        // mode 1 (phone) ~ offset 2
-        // mode 2 (tablet) ~ offset 0
-        mRotationMapOffset = 4 - 2 * config->volumeKeysRotationMode;
-    }
 }
 
 static void mapStemKey(int32_t keyCode, const PropertyMap& config, char const *property) {
@@ -2274,7 +2257,7 @@ static void mapStemKey(int32_t keyCode, const PropertyMap& config, char const *p
 }
 
 void KeyboardInputMapper::configureParameters() {
-    mParameters.orientationAware = !getDevice()->isExternal();
+    mParameters.orientationAware = false;
     const PropertyMap& config = getDevice()->getConfiguration();
     config.tryGetProperty(String8("keyboard.orientationAware"),
             mParameters.orientationAware);
@@ -2388,7 +2371,7 @@ void KeyboardInputMapper::processKey(nsecs_t when, bool down, int32_t scanCode,
     if (down) {
         // Rotate key codes according to orientation if needed.
         if (mParameters.orientationAware) {
-            keyCode = rotateKeyCode(keyCode, getOrientation(), mRotationMapOffset);
+            keyCode = rotateKeyCode(keyCode, getOrientation());
         }
 
         // Add key down.
